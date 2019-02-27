001da448955c1444181a5146e372319b2411cca8
Benoit Calin <benoit.calin@gmail.com>
2019-02-27 12:13:46 +0100

    Add simple form address & Fix controllers to update address

--- app/controllers/participations/accepteds_controller.rb
+++ app/controllers/participations/accepteds_controller.rb
@@ -1,7 +1,14 @@
 class Participations::AcceptedsController < ApplicationController
   def create
     participation = Participation.find(params[:participation_id])
+    participation.address = participation_params[:address]
     participation.accepted!
     redirect_to participation.event
+  end
+
+  private
+
+  def participation_params
+    params.require(:participation).permit(:address, :participation_id)
   end
 end
--- app/controllers/participations/declineds_controller.rb
+++ app/controllers/participations/declineds_controller.rb
@@ -1,6 +1,7 @@
 class Participations::DeclinedsController < ApplicationController
   def create
     participation = Participation.find(params[:participation_id])
+    participation.address = nil
     participation.declined!
     redirect_to participation.event
   end
