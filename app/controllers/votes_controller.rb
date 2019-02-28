d9635d3523e934de651427dfddd43aaf2ccc5cdd
nicolas beaudouin <nicolas.pn.beaudouin@gmail.com>
2019-02-28 12:52:52 +0100

    work in progress on voting2

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,6 +1,10 @@
 class VotesController < ApplicationController
   def create
     participation = Participation.find(params["participation_id"].to_i)
+    # place = sdlkfj
+    # parcicipation.place = place creer table places
+    # participation.save
+
     redirect_to event_path(participation.event)
   end
 end
