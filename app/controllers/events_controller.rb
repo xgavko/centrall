788540d38a467cc0d849f7403aaae1e9fad9615e
Benoit Calin <benoit.calin@gmail.com>
2019-02-28 18:07:40 +0100

    Sylvain

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,7 +2,18 @@ class EventsController < ApplicationController
   before_action :set_event, only: [:show]
 
   def show
-    Participation.create(user: current_user, event: @event) if @event.boarding?
+    if @event.created_at + 30.minutes < Time.current
+      @event.display_result!
+    elsif @event.created_at + 15.minutes < Time.current
+      @event.voting!
+    end
+
+    case @event.status
+    when "boarding"
+      Participation.create(user: current_user, event: @event)
+    when "voting"
+      @event.set_places unless @event.places.any?
+    end
   end
 
   private
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,5 @@
+require "open-uri"
+
 class Event < ApplicationRecord
   belongs_to :user
   has_many :participations, dependent: :destroy
@@ -8,4 +10,62 @@ class Event < ApplicationRecord
   validates :kind, presence: true
 
   enum status: { boarding: 0, voting: 1, display_result: 2 }
+
+  def barycenter
+    latitudes = []
+    longitudes = []
+    participations.each do |participation|
+      next if participation.latitude.nil?
+      next if participation.longitude.nil?
+
+      latitudes << participation.latitude
+      longitudes << participation.longitude
+    end
+    return nil if latitudes.empty?
+
+    [(latitudes.sum / latitudes.size), (longitudes.sum / longitudes.size)].join(',')
+  end
+
+  def set_places
+    location = "location=#{barycenter}"
+    rankby = "&rankby=distance"
+    type = "&type=#{kind}"
+    key = "&key=#{ENV['GOOGLE_PLACES_API_KEY']}"
+
+    parameters = "#{location}#{rankby}#{type}#{key}"
+
+    url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?#{parameters}"
+
+    places_serialized = open(url).read
+    places_parsed = JSON.parse(places_serialized)
+
+    places = places_parsed["results"].first(3)
+    places.each do |place|
+      data = {
+        google_id: place["place_id"],
+        name: place["name"],
+        rating: place["rating"]
+      }
+      create_place(data)
+    end
+  end
+
+  def create_place(data)
+    place = places.new(data)
+    details = get_details(place)
+    place.address = details["result"]["formatted_address"]
+    place.save
+  end
+
+  def get_details(establishment)
+    key = "&key=#{ENV['GOOGLE_PLACES_API_KEY']}"
+    fields = "&fields=formatted_address,icon,formatted_phone_number,website,reviews"
+
+    parameters2 = "place_id=#{establishment.google_id}#{fields}#{key}"
+
+    url_detail = "https://maps.googleapis.com/maps/api/place/details/json?#{parameters2}"
+
+    details_serialized = open(url_detail).read
+    JSON.parse(details_serialized)
+  end
 end
