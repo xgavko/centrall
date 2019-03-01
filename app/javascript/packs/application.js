6d01d8208514f38a96b4b53b8ad19986b2f7d19f
Benoit Calin <benoit.calin@gmail.com>
2019-03-01 11:04:10 +0100

    Modifs comment some Mapbox JS

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,12 +1,12 @@
 import "popper.js";
 import "bootstrap";
 
+// import 'mapbox-gl/dist/mapbox-gl.css';
+
+// import { initMapbox } from '../plugins/init_mapbox';
 import { initAutocomplete } from '../plugins/init_autocomplete.js';
 import {} from '../components/flyoutnavbar.js';
 import {} from '../components/geolocation.js';
 
-import 'mapbox-gl/dist/mapbox-gl.css';
-import { initMapbox } from '../plugins/init_mapbox';
-
+// initMapbox();
 initAutocomplete();
-initMapbox();
