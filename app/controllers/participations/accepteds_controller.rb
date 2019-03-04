b6a6d45db95d4b14b95d2e0192b3a9d2ef76546f
Benoit Calin <benoit.calin@gmail.com>
2019-03-04 15:36:25 +0100

    Add JS functionalities + error if address wrong

--- app/controllers/participations/accepteds_controller.rb
+++ app/controllers/participations/accepteds_controller.rb
@@ -1,14 +1,18 @@
 class Participations::AcceptedsController < ApplicationController
   def create
-    participation = Participation.find(params[:participation_id])
-    participation.address = participation_params[:address]
-    participation.accepted!
-    redirect_to participation.event
+    @participation = Participation.find(params[:participation_id])
+    @participation.address = participation_params[:address]
+    @participation.status = :accepted
+    @participation.save
+
+    respond_to do |format|
+      format.js # participations/accepteds/create.js
+    end
   end
 
   private
 
   def participation_params
-    params.require(:participation).permit(:address, :participation_id)
+    params.require(:participation).permit(:address)
   end
 end
--- app/models/participation.rb
+++ app/models/participation.rb
@@ -4,9 +4,26 @@ class Participation < ApplicationRecord
   belongs_to :place, optional: true
 
   validates :user, uniqueness: { scope: :event }
+  validate :address_on_acceptation
 
   enum status: { pending: 0, accepted: 1, declined: 2 }
 
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
+  after_validation  :lat_changed?
+
+  private
+
+  def lat_changed?
+    unless self.latitude_changed?
+      self.errors.add(:address, 'You need to add a proper address')
+    end
+  end
+
+
+  def address_on_acceptation
+    if accepted? && address.blank?
+      self.errors.add(:address, 'You need to add an address')
+    end
+  end
 end
