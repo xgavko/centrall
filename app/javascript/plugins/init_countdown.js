cb4bb9ed5fd6d0c7b1f7ee3260dc80fba2adbb7d
Xeniia Gavrilenko <X.gavrilenko@gmail.com>
2019-03-04 18:20:49 +0100

    added js moment, underscore and moment-duration-format

--- app/javascript/plugins/init_countdown.js
+++ app/javascript/plugins/init_countdown.js
@@ -3,13 +3,21 @@ import moment from "moment";
 import "moment-duration-format";
 
 
+const initCountdown = () => {
 var currentTime = moment().unix();
 var creationTime = moment(document.querySelector(".boarding-time").dataset.start_at).unix();
-var intervall = 1000;
+var interval = 1000;
 var endTime = creationTime + 15000;
-var duration = (endTime - currentTime) * 1000;
+var mainDiffTime = (endTime - currentTime) * 1000;
+var duration = moment.duration(mainDiffTime, 'milliseconds');
 
-const button = document.querySelector(".geoloc-submit")
+$('#main-passage .countdown').html(duration.format("hh:mm:ss"));
+
+const button = document.querySelector(".geoloc-submit");
+
 button.addEventListener('click', (event) => {
+  getElementById("boarding-3").html(duration.format("hh:mm:ss"));
+    }, interval);
+}
 
-});
+export { initCountdown };
