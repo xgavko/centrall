cfab3bbf906bde46d553eb2361d97e9f5267f44e
Xeniia Gavrilenko <X.gavrilenko@gmail.com>
2019-03-06 11:20:25 +0100

    debugged map not showing on last page

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -15,15 +15,21 @@ const initMapbox = () => {
       zoom: 15
     });
 
-
+    if (mapElement.dataset.markers) {
     const markers = JSON.parse(mapElement.dataset.markers);
-    markers.forEach(function(marker) {
-      new mapboxgl.Marker()
+      markers.forEach(function(marker) {
+        new mapboxgl.Marker()
+          .setLngLat([ marker.lng, marker.lat ])
+          .addTo(map);
+      });
+    };
+
+    if (mapElement.dataset.marker) {
+    const marker = JSON.parse(mapElement.dataset.marker);
+    new mapboxgl.Marker()
         .setLngLat([ marker.lng, marker.lat ])
         .addTo(map);
-    });
-
-
+    };
 
   }
 };
